2b52423ddcbb17ded65c3ae4c25e5bc7998ec75c
unknown <lucash.96@hotmail.com>
2025-02-09 22:28:06 -0300

    Adicionando AppLogin

---
+++ AppLogin/app/src/main/java/com/example/applogin/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.applogin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.applogin.ui.theme.AppLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Aprendendo a aplicar notificações
+        Toast.makeText(getApplicationContext(),
+            "Bem vindo ao sistema",
+            Toast.LENGTH_SHORT).show();
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppLoginTheme {
+        Greeting(name = "Android")
+    }
+}
